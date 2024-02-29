328da41fda54dd4cc898f25130cd47161fb6126d
Your Name <jochebedmshelia@gmail.com>
2024-02-29 21:10:56 +0100

    Initialed Spotify clone repo

---
+++ React/spotify/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      people of africa
+    </div>
+  )
+}
+export default App;
